b3c31c28e56007df35f4343f2507b10cd34afc05
saif bessassi <sa.bessassi@gmail.com>
2020-04-15 00:01:04 +0100

    init commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  MatCheckboxModule
+} from '@angular/material';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    MatCheckboxModule
+  ],
+})
+export class SharedModule { }
