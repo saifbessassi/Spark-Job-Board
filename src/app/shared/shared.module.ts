3d61ef55204d26d8ad978f8ccb45b7088a7494ea
saif bessassi <sa.bessassi@gmail.com>
2020-04-15 22:41:36 +0100

    ADD: angular truncate pipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TruncatePipesModule } from 'angular-truncate-pipes';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TruncatePipesModule
   ],
 })
 export class SharedModule { }
