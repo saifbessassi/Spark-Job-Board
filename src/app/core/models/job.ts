d70d128c5c561c67af2a44e260895c790e8cd411
saif bessassi <sa.bessassi@gmail.com>
2020-04-16 01:14:41 +0100

    ADD: job model

---
+++ src/app/core/models/job.ts
@@ -0,0 +1,18 @@
+export class Job {
+
+    public id: number;
+    public title: string;
+    public location: string;
+    public salary: number;
+    public description: string;
+    public employmentType: string;
+    public seniorityLevel: string;
+    public status: string;
+    public skills: string [];
+    public created_at: Date;
+    public updated_at: Date;
+
+    constructor(
+
+    ) {}
+}
