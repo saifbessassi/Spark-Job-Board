8c8abb12dbd851060c549da4b2a0718c95301f01
saif bessassi <sa.bessassi@gmail.com>
2020-04-16 15:12:40 +0100

    ADD: search bar component

--- src/app/job/job.module.ts
+++ src/app/job/job.module.ts
@@ -5,10 +5,11 @@ import { LandingComponent } from './pages/landing/landing.component';
 import { SharedModule } from '../shared/shared.module';
 import { CategoryBoxComponent } from './component/category-box/category-box.component';
 import { JobPostComponent } from './component/job-post/job-post.component';
+import { SearchBarComponent } from './component/search-bar/search-bar.component';
 
 
 @NgModule({
-  declarations: [LandingComponent, CategoryBoxComponent, JobPostComponent],
+  declarations: [LandingComponent, CategoryBoxComponent, JobPostComponent, SearchBarComponent],
   imports: [
     JobRoutingModule,
     SharedModule
