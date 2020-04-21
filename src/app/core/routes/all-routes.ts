ac2310add017521b336ae956c840cc3e06444f64
saif bessassi <sa.bessassi@gmail.com>
2020-04-21 17:36:03 +0100

    ADD: authentication module

--- src/app/core/routes/all-routes.ts
+++ src/app/core/routes/all-routes.ts
@@ -5,5 +5,10 @@ export const ALL_ROUTES: Routes = [
         path: '',
         loadChildren: () => import('src/app/job/job.module')
         .then(m => m.JobModule),
+    },
+    {
+        path: 'auth',
+        loadChildren: () => import('src/app/authentication/authentication.module')
+        .then(m => m.AuthenticationModule),
     }
 ];
