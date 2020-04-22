7619382f84b5889a9cba52fc0cba71161d41ee14
saif bessassi <sa.bessassi@gmail.com>
2020-04-22 17:46:55 +0100

    MOD: display connected user avatar and name

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenService } from '../../services/token/token.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'sp-header',
@@ -7,11 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  public isCollapsed = true;
+  fullname: string;
+  email: string;
+  photo: string;
+  isValid: boolean;
+  nameTwoLetter: string;
   
-  constructor() { }
+  constructor(
+    private router: Router,
+    private tokenService: TokenService,
+  ) { }
 
   ngOnInit() {
+    this.isValid = this.tokenService.isValid();
+    if (this.isValid) {
+      const payload = this.tokenService.getPayload();
+      this.email = payload['username'];
+      this.fullname = payload['fullname'];
+      this.photo = payload['photo'];
+      if (this.fullname.indexOf(' ') > -1) {
+        this.nameTwoLetter = this.fullname[0] + this.fullname.split(' ')[1][0];
+      } else {
+        this.nameTwoLetter = this.fullname[0] + this.fullname[1];
+      }
+      
+    }
   }
 
 }
