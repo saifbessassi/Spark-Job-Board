07dcc24549bb4bff00e99e603171bf441f66ee9e
saif bessassi <sa.bessassi@gmail.com>
2020-04-23 00:47:36 +0100

    FIX: signup with google and facebook

--- src/app/authentication/components/signup/signup.component.ts
+++ src/app/authentication/components/signup/signup.component.ts
@@ -3,7 +3,7 @@ import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
 import { AuthenticationService } from 'src/app/core/services/auth/authentication.service';
 import { Router } from '@angular/router';
 import { TokenService } from 'src/app/core/services/token/token.service';
-import { AuthService as SocialAuthService, GoogleLoginProvider } from 'angularx-social-login';
+import { AuthService as SocialAuthService, GoogleLoginProvider, FacebookLoginProvider } from 'angularx-social-login';
 import { PasswordValidator } from 'src/app/core/services/auth/passwordValidator.service';
 
 @Component({
@@ -58,7 +58,7 @@ export class SignupComponent implements OnInit {
     this.authenticationService.signup(candidate).subscribe(res => {
         this.success_msg = 'You have successfully registered. Please check your email inbox to verify your account';
         setTimeout(() => {
-            this.router.navigate(['candidate/landing']);
+            this.router.navigate(['auth/signin']);
         }, 6000);
         this.isLoading = false;
     }, errors => {
@@ -69,28 +69,41 @@ export class SignupComponent implements OnInit {
     });
   }
 
-  // public socialSignUp(provider) {
-  //     this.authenticationService.connect(provider);
-  // }
-
-  public socialSignUp(socialProvider: string) {
-    let socialPlatformProvider;
-    socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
+  socialSignUp(provider: string) {
     this.isLoading = true;
-    this.OAuth.signIn(socialPlatformProvider).then(googleUser => {
-      // TODO: send token and provider name to backend
-      this.authenticationService.socialLogin(googleUser.idToken, 'google').subscribe( (res: {access_token: string, refresh_token: string}) => {
-        this.tokenService.setToken(res.access_token);
-        this.tokenService.setRefreshToken(res.refresh_token);
-        this.success_msg = 'You have been successfully logged in.';
-        this.router.navigate(['/']);
-        this.isLoading = false;
-      }, err => {
-        this.error_msg = err;
-        this.isLoading = false;
-      });
+    let providerID;
+    switch (provider) {
+      case 'google':
+        providerID = GoogleLoginProvider.PROVIDER_ID;
+        break;
+      case 'facebook':
+        providerID = FacebookLoginProvider.PROVIDER_ID;
+        break;
+      default:
+        break;
+    }
+    
+    this.OAuth.signIn(providerID).then(user => {
+      let token = user.authToken;
+      if(provider === 'google') {
+        token = user.idToken;
+      }
+      this.socialSignupReq(token, provider);
     }, err => {
       this.error_msg = err;
+      this.isLoading = false;
+    });
+  }
+
+  socialSignupReq(token: string, provider: string) {
+    this.authenticationService.socialLogin(token, provider).subscribe( (res: {access_token: string, refresh_token: string}) => {
+      this.tokenService.setToken(res.access_token);
+      this.tokenService.setRefreshToken(res.refresh_token);
+      this.router.navigate(['/']);
+      this.isLoading = false;
+    }, err => {
+      console.log('ok')
+      this.error_msg = err.error;
       this.isLoading = false;
     });
   }
