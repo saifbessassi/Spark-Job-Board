8747433cb8688a8e5e8582d979b15b1ee4781570
saif bessassi <sa.bessassi@gmail.com>
2020-05-13 20:50:01 +0100

    MOD: add refresh token

--- src/app/core/interceptors/http-token.interceptor.ts
+++ src/app/core/interceptors/http-token.interceptor.ts
@@ -3,33 +3,81 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse
 } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { Observable, BehaviorSubject, throwError } from "rxjs";
 import { TokenService } from '../services/token/token.service';
+import { catchError, filter, take, switchMap } from 'rxjs/operators';
+import { AuthenticationService } from '../services/auth/authentication.service';
 
 @Injectable({
   providedIn: "root"
 })
 export class HttpTokenInterceptor implements HttpInterceptor {
+
+  private isRefreshing = false;
+  private refreshTokenSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+
   constructor(
-    private tokenService: TokenService
-  ) {}
+    private tokenService: TokenService,
+    private authService: AuthenticationService
+  ) { }
 
-  intercept(
-    request: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
-    // add authorization header with jwt token if available
-    const token = this.tokenService.getAccessToken();
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
+    if (this.tokenService.getAccessToken() && !request.url.includes('refresh')) {
+      request = this.addToken(request, this.tokenService.getAccessToken());
+    }
+
+    return next.handle(request).pipe(catchError(error => {
+      if (error instanceof HttpErrorResponse && error.status === 401) {
+        if (error.url.includes('refresh')) {
+          this.authService.logout();
+          let header = request.headers.delete('Authorization');
+          request.clone({headers: header});
+          return next.handle(request);
+        }
+        return this.handle401Error(request, next);
+      } else {
+        return throwError(error);
+      }
+    }));
+  }
+
+  private addToken(request: HttpRequest<any>, token: string) {
     if (token) {
-      request = request.clone({
+      return request.clone({
         setHeaders: {
-          Authorization: "Bearer " + token
+          'Authorization': `Bearer ${token}`
         }
       });
+    } else {
+      return request.clone();
     }
-    return next.handle(request);
+    
   }
+
+  private handle401Error(request: HttpRequest<any>, next: HttpHandler) {
+    if (!this.isRefreshing) {
+      this.isRefreshing = true;
+      this.refreshTokenSubject.next(null);
+      return this.tokenService.refreshToken().pipe(
+        switchMap((token) => {
+          this.isRefreshing = false;
+          this.refreshTokenSubject.next(token.token);
+          return next.handle(this.addToken(request, token.token));
+        }));
+
+    } else {
+      return this.refreshTokenSubject.pipe(
+        filter(token => token != null),
+        take(1),
+        switchMap(jwt => {
+          return next.handle(this.addToken(request, jwt));
+        }));
+    }
+  }
+
+
 }
--- src/app/core/services/token/token.service.ts
+++ src/app/core/services/token/token.service.ts
@@ -1,13 +1,27 @@
 import { Injectable } from '@angular/core';
 import * as jwt_decode from 'jwt-decode';
 import { Token } from '../../models/token.model';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+
+
+const API_URL = environment.API_URL;
+
+interface RefreshResponse {
+  token: string;
+  refresh_token: string
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class TokenService {
   
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   getAccessToken() {
     const token = JSON.parse(localStorage.getItem('token'));
@@ -30,7 +44,21 @@ export class TokenService {
     return null;
   }
 
-  clear() {
-    localStorage.clear();
+  refreshToken() {
+    const refresh = {
+      'refresh_token': this.getRefreshToken()
+    }
+    return this.http.post<RefreshResponse>(API_URL + '/api/token/refresh', refresh)
+      .pipe(tap((data) => {
+        const payload = jwt_decode(data.token);
+        let token = new Token();
+  
+        token.access_token = data.token;
+        token.refrech_token = data.refresh_token;
+        token.exp = payload['exp'];
+        token.iat = payload['iat'];
+        localStorage.setItem('token', JSON.stringify(token));
+      }));
   }
+
 }
