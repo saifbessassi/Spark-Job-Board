e5dde424959e33fac503700c963e91a0121f7172
saif bessassi <sa.bessassi@gmail.com>
2020-05-17 07:14:45 +0100

    ADD: add new job page

--- src/app/recruiter/recruiter-routing.module.ts
+++ src/app/recruiter/recruiter-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RecruiterLayoutComponent } from './layout/recruiter-layout/recruiter-layout.component';
 import { JobsTableComponent } from './pages/jobs-table/jobs-table.component';
+import { AddJobComponent } from './pages/add-job/add-job.component';
 
 
 const routes: Routes = [
@@ -12,6 +13,10 @@ const routes: Routes = [
       {
         path: 'jobs-list',
         component: JobsTableComponent
+      },
+      {
+        path: 'add-job',
+        component: AddJobComponent
       }
     ]
   }
--- src/app/recruiter/recruiter.module.ts
+++ src/app/recruiter/recruiter.module.ts
@@ -6,18 +6,23 @@ import { JobsTableComponent } from './pages/jobs-table/jobs-table.component';
 import { Ng2SmartTableModule } from 'ng2-smart-table';
 import { RecruiterLayoutComponent } from './layout/recruiter-layout/recruiter-layout.component';
 import { SharedModule } from '../shared/shared.module';
-
+import { JobFormComponent } from './components/job-form/job-form.component';
+import { AddJobComponent } from './pages/add-job/add-job.component';
+import { EditorModule } from '@tinymce/tinymce-angular';
 
 @NgModule({
   declarations: [
     JobsTableComponent,
-    RecruiterLayoutComponent
+    RecruiterLayoutComponent,
+    JobFormComponent,
+    AddJobComponent
   ],
   imports: [
     CommonModule,
     RecruiterRoutingModule,
     Ng2SmartTableModule,
-    SharedModule
+    SharedModule,
+    EditorModule
   ],
   providers: [
     DatePipe
