285fbdb56e3a83ec54a7ac5e4132380396c2e146
saif bessassi <sa.bessassi@gmail.com>
2020-06-03 18:39:06 +0100

    test

--- src/app/core/components/sidebar/sidebar.component.spec.ts
+++ src/app/core/components/sidebar/sidebar.component.spec.ts
@@ -1,14 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SidebarComponent } from './sidebar.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { RouterLinkDirectiveStub } from 'src/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('SidebarComponent', () => {
   let component: SidebarComponent;
   let fixture: ComponentFixture<SidebarComponent>;
+  let routerLinks: RouterLinkDirectiveStub[];
+  let linkDes: DebugElement[];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SidebarComponent ]
+      declarations: [ 
+        SidebarComponent,
+        RouterLinkDirectiveStub 
+      ]
     })
     .compileComponents();
   }));
@@ -17,9 +26,32 @@ describe('SidebarComponent', () => {
     fixture = TestBed.createComponent(SidebarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    linkDes = fixture.debugElement.queryAll(By.directive(RouterLinkDirectiveStub));
+    routerLinks = linkDes.map(de => de.injector.get(RouterLinkDirectiveStub));
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('can get RouterLinks from template', () => {
+    expect(routerLinks.length).toBe(4, 'should have 4 routerLinks');
+    expect(routerLinks[0].linkParams).toBe('/recruiter/dashboard');
+    expect(routerLinks[1].linkParams).toBe('/recruiter/jobs-list');
+    expect(routerLinks[2].linkParams).toBe('/recruiter/add-job');
+    expect(routerLinks[3].linkParams).toBe('/recruiter/all-candidates');
+  });
+
+  it('can click Dashboard link in template', () => {
+    const dashboardLinkDe = linkDes[0];   // dashboard link DebugElement
+    const dashboardLink = routerLinks[0]; // dashboard link directive
+
+    expect(dashboardLink.navigatedTo).toBeNull('should not have navigated yet');
+
+    dashboardLinkDe.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(dashboardLink.navigatedTo).toBe('/recruiter/dashboard');
+  });
 });
+
