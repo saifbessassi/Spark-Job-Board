53858ab6f4e47666e7fd0c9199517a2c5d22861f
saif bessassi <sa.bessassi@gmail.com>
2020-06-03 16:40:22 +0100

    test

--- src/app/core/components/top-bar/top-bar.component.spec.ts
+++ src/app/core/components/top-bar/top-bar.component.spec.ts
@@ -1,25 +1,46 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { TopBarComponent } from './top-bar.component';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { ALL_ROUTES } from '../../routes/all-routes';
+import { click } from 'src/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { RouterLinkWithHref } from '@angular/router';
+import { SpyLocation }         from '@angular/common/testing';
+import { AppLayoutComponent } from '../../layouts/app-layout/app-layout.component';
 
 describe('TopBarComponent', () => {
   let component: TopBarComponent;
   let fixture: ComponentFixture<TopBarComponent>;
+  let homeLinkDeb: DebugElement;
+  let location: SpyLocation;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TopBarComponent ]
+      imports: [
+        RouterTestingModule.withRoutes(ALL_ROUTES),
+      ]
     })
     .compileComponents();
-  }));
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(TopBarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should navigate to "Home" on click', fakeAsync(() => {
+    const links = fixture.debugElement.queryAll(By.directive(RouterLinkWithHref));
+    this.homeLinkDeb = links[0];
+    click(homeLinkDeb);
+
+    tick(); // wait while navigating
+    fixture.detectChanges(); // update view
+    tick(); // wait for async data to arrive
+
+    expect(location.path()).toEqual('/about', 'location.path()');
+    const el = fixture.debugElement.query(By.directive(AppLayoutComponent));
+    expect(el).toBeTruthy('expected an element for ' + AppLayoutComponent.name);
+  }));
 });
