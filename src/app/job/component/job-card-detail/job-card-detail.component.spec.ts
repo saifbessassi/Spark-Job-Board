ee0524a15283aa87dc1a2664af6522692b923b69
saif bessassi <sa.bessassi@gmail.com>
2020-06-04 22:05:41 +0100

    test

--- src/app/job/component/job-card-detail/job-card-detail.component.spec.ts
+++ src/app/job/component/job-card-detail/job-card-detail.component.spec.ts
@@ -1,21 +1,45 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { JobCardDetailComponent } from './job-card-detail.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { AuthenticationService } from 'src/app/core/services/auth/authentication.service';
+import { UserService } from 'src/app/core/services/user/user.service';
+import { of } from 'rxjs';
+import { User } from 'src/app/core/models/user.service';
+import { Job } from 'src/app/core/models/job';
 
 describe('JobCardDetailComponent', () => {
   let component: JobCardDetailComponent;
   let fixture: ComponentFixture<JobCardDetailComponent>;
+  const modalSpy = jasmine.createSpyObj('NgbModal', ['close']);
+  let authService: AuthenticationService;
+  let isCandidateSpy: jasmine.Spy;
+  let isRecruiterSpy: jasmine.Spy;
+  let userService;
+  let user = new User;
+  let job = new Job;
+  user.appliedJobs = [1,2,3];
 
   beforeEach(async(() => {
+    userService = jasmine.createSpyObj('UserService', ['isCandidate', 'isRecruiter']);
     TestBed.configureTestingModule({
-      declarations: [ JobCardDetailComponent ]
+      declarations: [ JobCardDetailComponent ],
+      providers: [
+        { provide: NgbModal, useValue: modalSpy },
+        {provide: AuthenticationService, useValue: {currentUser: of(user)}},
+        {provide: UserService, useValue: userService}
+      ]
     })
     .compileComponents();
+    isCandidateSpy = userService.isCandidate.and.returnValue(true);
+    isRecruiterSpy = userService.isRecruiter.and.returnValue(false);
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(JobCardDetailComponent);
     component = fixture.componentInstance;
+    job.id = 1;
+    component.job = job;
     fixture.detectChanges();
   });
 
--- src/app/job/component/job-card-detail/job-card-detail.component.ts
+++ src/app/job/component/job-card-detail/job-card-detail.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Job } from 'src/app/core/models/job';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ApplyModalComponent } from '../apply-modal/apply-modal.component';
-import { ApplicationService } from 'src/app/core/services/application/application.service';
 import { UserService } from 'src/app/core/services/user/user.service';
 import { AuthenticationService } from 'src/app/core/services/auth/authentication.service';
 import { User } from 'src/app/core/models/user.service';
@@ -20,7 +19,6 @@ export class JobCardDetailComponent implements OnInit {
 
   constructor(
     private modalService: NgbModal,
-    private applyService: ApplicationService,
     private userService: UserService,
     private authService: AuthenticationService
   ) {
