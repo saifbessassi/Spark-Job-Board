78a65b0c669fdd0f9b297524868406ecac272e6c
saif bessassi <sa.bessassi@gmail.com>
2020-06-04 17:01:40 +0100

    test

--- src/app/job/component/category-box/category-box.component.spec.ts
+++ src/app/job/component/category-box/category-box.component.spec.ts
@@ -1,14 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CategoryBoxComponent } from './category-box.component';
+import { Router } from '@angular/router';
 
 describe('CategoryBoxComponent', () => {
   let component: CategoryBoxComponent;
   let fixture: ComponentFixture<CategoryBoxComponent>;
+  const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CategoryBoxComponent ]
+      declarations: [ CategoryBoxComponent ],
+      providers: [
+        { provide: Router, useValue: routerSpy }
+      ]
     })
     .compileComponents();
   }));
@@ -21,5 +26,17 @@ describe('CategoryBoxComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate with expected param', () => {
+    component.label = 'category 1';
+    const catBoxElement: HTMLElement = fixture.nativeElement;
+    const button = catBoxElement.querySelector('button');
+    button.click();
+    const spy = routerSpy.navigate as jasmine.Spy;
+    const navArgs = spy.calls.first();
+    const url = navArgs.args[0];
+    const urlParam = navArgs.args[1].state.data[0];
+    expect(urlParam.value).toEqual('category 1');
   });
 });
