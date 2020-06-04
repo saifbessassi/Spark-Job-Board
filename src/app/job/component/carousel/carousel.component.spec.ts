fee3995427200d433a764c4676dd074bdc81a8a4
saif bessassi <sa.bessassi@gmail.com>
2020-06-04 17:08:55 +0100

    test

--- src/app/job/component/carousel/carousel.component.spec.ts
+++ src/app/job/component/carousel/carousel.component.spec.ts
@@ -22,4 +22,17 @@ describe('CarouselComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display expected title and text', () => {
+    const expectedTitle = 'this is a title';
+    const expectedtext = 'text text text text text text ';
+    component.text = expectedtext;
+    component.title = expectedTitle;
+    fixture.detectChanges();
+    const carouselElement: HTMLElement = fixture.nativeElement;
+    const h2 = carouselElement.querySelector('h2');
+    const p = carouselElement.querySelector('p');
+    expect(h2.textContent).toEqual(expectedTitle, 'should display expected title');
+    expect(p.textContent).toEqual(expectedtext, 'should display expected text');
+  })
 });
