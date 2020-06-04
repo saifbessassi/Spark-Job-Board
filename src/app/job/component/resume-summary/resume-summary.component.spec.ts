960779b68c432964c5001fcc6a97bf7874325215
saif bessassi <sa.bessassi@gmail.com>
2020-06-04 20:33:12 +0100

    test

--- src/app/job/component/resume-summary/resume-summary.component.spec.ts
+++ src/app/job/component/resume-summary/resume-summary.component.spec.ts
@@ -1,14 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ResumeSummaryComponent } from './resume-summary.component';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
-describe('EditResumeFormComponent', () => {
+@Component({selector: 'ngb-progressbar', template: ''})
+export class ProgressbarStubComponent{@Input() value: number}
+
+describe('ResumeSummaryComponent', () => {
   let component: ResumeSummaryComponent;
   let fixture: ComponentFixture<ResumeSummaryComponent>;
+  const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
+  const activeModalSpy = jasmine.createSpyObj('NgbActiveModal', ['close']);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ResumeSummaryComponent ]
+      declarations: [ 
+        ResumeSummaryComponent,
+        ProgressbarStubComponent
+       ],
+       providers: [
+         { provide: Router, useValue: routerSpy },
+         { provide: NgbActiveModal, useValue: activeModalSpy }
+        ]
     })
     .compileComponents();
   }));
