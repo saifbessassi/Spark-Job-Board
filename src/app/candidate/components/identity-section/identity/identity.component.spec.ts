192c19208adb9381d32c3e1571e1dc6673d0dfb7
saif bessassi <sa.bessassi@gmail.com>
2020-06-09 13:35:11 +0100

    test

--- src/app/candidate/components/identity-section/identity/identity.component.spec.ts
+++ src/app/candidate/components/identity-section/identity/identity.component.spec.ts
@@ -1,14 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { IdentityComponent } from './identity.component';
+import { NgbModule, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { IdentityFormComponent } from '../identity-form/identity-form.component';
+
+export class MockNgbModalRef {
+  componentInstance = {
+      description: undefined,
+      resumeID: undefined
+  };
+  result: Promise<any> = new Promise((resolve, reject) => resolve('description'));
+}
 
 describe('IdentityComponent', () => {
   let component: IdentityComponent;
   let fixture: ComponentFixture<IdentityComponent>;
+  let ngbModal;
+  let mockModalRef: MockNgbModalRef = new MockNgbModalRef();
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ IdentityComponent ]
+      declarations: [ IdentityComponent ],
+      imports: [
+        NgbModule
+      ],
     })
     .compileComponents();
   }));
@@ -16,10 +31,23 @@ describe('IdentityComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(IdentityComponent);
     component = fixture.componentInstance;
+    ngbModal = TestBed.get(NgbModal);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should open modal', () => {
+    spyOn(ngbModal, 'open').and.returnValue(mockModalRef);
+    component.openEditForm();
+    expect(ngbModal.open).toHaveBeenCalledWith(
+      IdentityFormComponent, 
+      { 
+        centered: true, 
+        size: 'lg'
+      }
+    );
   });
 });
