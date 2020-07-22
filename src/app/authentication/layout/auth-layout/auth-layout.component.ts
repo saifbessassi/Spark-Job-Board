ba581adaaccd395f5b7dfd12171e5d56a7df88ce
Saif BESSASSI <sbessassi@spark.local>
2020-07-22 10:42:42 +0100

    add button to change language

--- src/app/authentication/layout/auth-layout/auth-layout.component.ts
+++ src/app/authentication/layout/auth-layout/auth-layout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'sp-auth-layout',
@@ -9,8 +10,14 @@ import { Location } from '@angular/common';
 export class AuthLayoutComponent {
 
   constructor(
-    private location: Location
-  ) { }
+    private location: Location,
+    public translate: TranslateService
+  ) {
+    translate.addLangs(['en', 'fr']);
+    translate.setDefaultLang('en');
+    const browserLang = translate.getBrowserLang();
+    translate.use(browserLang.match(/en|fr/) ? browserLang : 'en');
+  }
 
   goBack() {
     this.location.back();
